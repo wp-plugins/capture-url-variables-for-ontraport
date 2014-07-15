43214a6e19b4091f02376b0e054bb22ae55fe129
itmooti <itmooti@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-15 01:13:25 +0000

    fancy new feature: now you can foo *and* bar at the same time
    git-svn-id: https://plugins.svn.wordpress.org/capture-url-variables-for-ontraport/tags/1.0.7@948449 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- capture-url-variables-for-ontraport.php
+++ capture-url-variables-for-ontraport.php
@@ -3,8 +3,8 @@
  * Plugin Name: OAP UTM WP Plugin
  * Plugin URI: http://www.itmooti.com/
  * Description: A plugin to add UTM and Referring Page fields on Ontraport Smart Forms
- * Version: 1.0.6
- * Stable tag: 1.0.6
+ * Version: 1.0.7
+ * Stable tag: 1.0.7
  * Author: ITMOOTI
  * Author URI: http://www.itmooti.com/
  */
